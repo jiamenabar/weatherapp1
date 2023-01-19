27eb736b945c9de099e93b63c127f7e96912eebc
Jose Ignacio Amenabar <jose.ignacio.amenabar@gmail.com>
2023-01-19 09:49:16 -0500

    added cities

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from myapp.models import WeatherApp
 
 # Register your models here.
+
+class WeatherAdmin(admin.ModelAdmin):
+    fields = ('city','country','weather')
+    model = WeatherApp
+    extra = 0
+
+admin.site.register(WeatherApp,WeatherAdmin)
