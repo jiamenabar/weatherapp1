1d5924b39186f136143998bfc6f3c75d494a288b
Jose Ignacio Amenabar <jose.ignacio.amenabar@gmail.com>
2023-01-19 15:03:19 -0500

    Add API

--- weatherapp1/urls.py
+++ weatherapp1/urls.py
@@ -19,6 +19,6 @@ from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home, name="home"),
+    path('home',views.home, name="home"),
     path('', views.index),  # the path for our index view,
 ]
