103b90fa25ca89063f2404b08bc81d2afaf369dc
Jose Ignacio Amenabar <jose.ignacio.amenabar@gmail.com>
2023-01-19 03:59:48 -0500

    Initial commit

--- myapp/models.py
+++ myapp/models.py
@@ -5,6 +5,8 @@ class WeatherApp(models.Model):
     city = models.CharField(max_length=20)
     country = models.CharField(max_length=50)
     weather = models.FloatField(default=0.0)
+    class Meta:
+        db_table = 'weatherapp2023'
     def __str__(self):
         return self.city + " " + self.country + " " + str(self.weather)
     def __repr__(self):
