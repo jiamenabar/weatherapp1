a94569ff7b6aabf3f437d1abc92f0a89b91530b4
Jose Ignacio Amenabar <jose.ignacio.amenabar@gmail.com>
2023-01-19 10:16:44 -0500

    Admin changes

--- myapp/admin.py
+++ myapp/admin.py
@@ -9,3 +9,11 @@ class WeatherAdmin(admin.ModelAdmin):
     extra = 0
 
 admin.site.register(WeatherApp,WeatherAdmin)
+
+from django.contrib.auth.models import User
+
+admin.site.unregister(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
