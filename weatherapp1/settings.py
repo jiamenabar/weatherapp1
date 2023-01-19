ef9caa0029777bee00379a60c99f4eee0ddcf2be
Jose Ignacio Amenabar <jose.ignacio.amenabar@gmail.com>
2023-01-19 13:22:44 -0500

    Admin changes

--- weatherapp1/settings.py
+++ weatherapp1/settings.py
@@ -77,7 +77,8 @@ WSGI_APPLICATION = 'weatherapp1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.config(default='postgresql://postgres:p5nfvw92o@localhost:5432/weatherapp2023', conn_max_age=600)}
+#DATABASES = {'default': dj_database_url.config(default='postgresql://postgres:p5nfvw92o@localhost:5432/weatherapp2023', conn_max_age=600)}
+DATABASES = {'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/weatherapp2023', conn_max_age=600)}
 
 
 # Password validation
